ef88dbc146e680d980c7bb8d70b24d0b780b69e6
1805674660 <1805674660@qq.com>
2020-08-28 19:43:26 +0800

    登录完善

--- js/axios.defaults.js
+++ js/axios.defaults.js
@@ -1,18 +1,21 @@
 //对ajax进行二次封装
-axios.defaults.baseURL = "http://localhost:8888"
+axios.defaults.baseURL = "http://127.0.0.1:8888";
 
 //数据以表单的形式给服务器
-axios.defaults.headers['Content-Type'] = 'addlication/x-www-form-urlencoded'
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+//配置为true 后台的请求都会带上 
+axios.defaults.withCredentials = true
 
 //数据格式name= 3&age = 4
-axios.defaults.transformRequset = function(data){
-    if(!data) return data;
-    let result = ""
-    for(let attr in data){
-        if (!data.hasOwnProperty(attr)) break
-        result +=`&${attr}=${data[attr]}`
+axios.defaults.transformRequest = function (data) {
+    if (!data) return data;
+    let result = ``;
+    for (let attr in data) {
+        if (!data.hasOwnProperty(attr)) break;
+        result += `&${attr}=${data[attr]}`;
     }
-}
+    return result.substring(1);
+};
 
 //axios请求拦截器
 axios.interceptors.request.use(config =>{
